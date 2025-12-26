3d7e8b7b731eb496561aa449c82f0cc39c7cca8d
Mattias Joëlson <mattias@joelson.org>
2025-12-26 17:04:26 +0100

    Spring initializer from https://start.spring.io/

---
+++ src/main/java/org/joelson/uitest/UitestApplication.java
@@ -0,0 +1,12 @@
+package org.joelson.uitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UitestApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(UitestApplication.class, args);
+    }
+}
---
+++ src/test/java/org/joelson/uitest/UitestApplicationTests.java
@@ -0,0 +1,12 @@
+package org.joelson.uitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UitestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}

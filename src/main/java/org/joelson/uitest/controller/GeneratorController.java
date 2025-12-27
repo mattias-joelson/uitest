915ea363a4716cc6d7bacd4fd219edbd80cf2fac
Mattias Joëlson <mattias@joelson.org>
2025-12-27 23:35:37 +0100

    Passing the model back and forth.

--- src/main/java/org/joelson/uitest/controller/GeneratorController.java
+++ src/main/java/org/joelson/uitest/controller/GeneratorController.java
@@ -4,13 +4,28 @@ import org.joelson.uitest.model.Generator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GeneratorController {
 
+    private Generator generator = null;
+
     @GetMapping("/generator")
     public String generator(Model model) {
-        model.addAttribute("generator", defaultGenerator());
+        model.addAttribute("generator", this.generator = defaultGenerator());
+        return "generator";
+    }
+
+    @PostMapping("/generator")
+    public String updateGenerator(Model model, Generator that) {
+        if (that == null) {
+            System.out.println("that = null, defaulting");
+            generator = defaultGenerator();
+        } else if (!that.getName().equals(generator.getName()) || that.getCount() != generator.getCount()) {
+            generator = that;
+        }
+        model.addAttribute("generator", generator);
         return "generator";
     }
 

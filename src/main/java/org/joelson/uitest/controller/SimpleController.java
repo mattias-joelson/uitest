0720688a6a0e90ef796a3ac2e93c8def122c46b0
Mattias Joëlson <mattias@joelson.org>
2025-12-27 13:58:40 +0100

    A simple page the forwards property settings to the UI.

---
+++ src/main/java/org/joelson/uitest/controller/SimpleController.java
@@ -0,0 +1,19 @@
+package org.joelson.uitest.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SimpleController {
+
+    @Value("${spring.application.name}")
+    String appName;
+
+    @RequestMapping("/simple")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
